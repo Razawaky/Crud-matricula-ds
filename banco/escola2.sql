8eb74adf77f544d111d78aaec3f357d4a074098d
Razawaky <viniciusduarte564@gmail.com>
2023-11-10 09:24:10 -0300

    banco

---
+++ banco/escola2.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 08-Nov-2023 às 12:13
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `escola`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno`
+--
+
+CREATE TABLE `aluno` (
+  `codigo` int(11) NOT NULL,
+  `nome_al` varchar(55) NOT NULL,
+  `idade` int(11) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `escolaridade` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`codigo`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `aluno`
+--
+ALTER TABLE `aluno`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- escola.sql
+++ escola.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08-Nov-2023 às 12:13
+-- Tempo de geração: 08-Nov-2023 às 11:57
 -- Versão do servidor: 10.4.20-MariaDB
 -- versão do PHP: 8.0.8
 
@@ -33,7 +33,34 @@ CREATE TABLE `aluno` (
   `idade` int(11) NOT NULL,
   `cpf` int(11) NOT NULL,
   `escolaridade` varchar(55) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `curso`
+--
+
+CREATE TABLE `curso` (
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(55) NOT NULL,
+  `area` varchar(55) NOT NULL,
+  `descricao` longtext NOT NULL,
+  `carga_horaria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `matricula`
+--
+
+CREATE TABLE `matricula` (
+  `cod_matricula` int(11) NOT NULL,
+  `data` varchar(55) NOT NULL,
+  `cod_aluno` int(11) DEFAULT NULL,
+  `cod_curso` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tabelas despejadas
@@ -43,8 +70,21 @@ CREATE TABLE `aluno` (
 -- Índices para tabela `aluno`
 --
 ALTER TABLE `aluno`
-  ADD PRIMARY KEY (`codigo`),
-  ADD UNIQUE KEY `cpf` (`cpf`);
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Índices para tabela `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Índices para tabela `matricula`
+--
+ALTER TABLE `matricula`
+  ADD PRIMARY KEY (`cod_matricula`),
+  ADD KEY `fk_mat_al` (`cod_aluno`),
+  ADD KEY `fk_mat_cur` (`cod_curso`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -54,7 +94,30 @@ ALTER TABLE `aluno`
 -- AUTO_INCREMENT de tabela `aluno`
 --
 ALTER TABLE `aluno`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `matricula`
+--
+ALTER TABLE `matricula`
+  MODIFY `cod_matricula` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `matricula`
+--
+ALTER TABLE `matricula`
+  ADD CONSTRAINT `fk_mat_al` FOREIGN KEY (`cod_aluno`) REFERENCES `aluno` (`codigo`),
+  ADD CONSTRAINT `fk_mat_cur` FOREIGN KEY (`cod_curso`) REFERENCES `curso` (`codigo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
