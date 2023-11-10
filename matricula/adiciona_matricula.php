1a87005949342f26691578a21f5d3f8ea1dd9e13
Razawaky <viniciusduarte564@gmail.com>
2023-11-10 09:35:48 -0300

    aaa

--- matricula/adiciona_matricula.php
+++ matricula/adiciona_matricula.php
@@ -7,13 +7,14 @@
 </head>
 <body>
     
+
 <form action="processa_matricula.php" method="post">
     <label for="">Data da matricula</label>
     <input type="text" name="data" placeholder="Digite a data">
     <select name="escolha_aluno">
         <option>Selecione um aluno:</option>
         <?php 
-            include 'conexao.php'
+            include 'conexao.php';
             $consulta = "SELECT * FROM aluno";
             $consulta_aluno = mysqli_query($conexao, $consulta);
             while($linha = mysqli_fetch_array($consulta_aluno)) {
@@ -26,7 +27,7 @@
     <select name = "escolha_curso">
     <option>Selecione um aluno:</option>
         <?php 
-            include 'conexao.php'
+            include 'conexao.php';
             $consulta = "SELECT * FROM aluno";
             $consulta_curso = mysqli_query($conexao, $consulta);
             while($linha = mysqli_fetch_array($consulta_aluno)) {
